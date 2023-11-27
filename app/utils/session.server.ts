0820b1950145ebc06f3e28ce6019a2b41d3547f4
michaeltaman <mictrwork@gmail.com>
2023-11-27 16:40:08 +0200

    changes in session.server

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -1,26 +1,14 @@
 import { createCookieSessionStorage } from "@remix-run/node";
 import {createThemeSessionResolver} from "remix-themes";
 
-/*const sessionStorage = createCookieSessionStorage({
-    cookie: {
-      name: "__remix-themes",
-      domain: process.env.NODE_ENV !== "development" ? "asdf": null,
-      path: "/",
-      httpOnly: true,
-      sameSites: "lax",
-      secrets: ["secert"],
-      secure: process.env.NODE_ENV === "development" ? false : true,
-    },
-  });*/
-
 const sessionStorage = createCookieSessionStorage({
     cookie: {
       name: "__remix-themes",
       path: "/",
       httpOnly: true,
-      //sameSites: "lax",
-      secrets: ["secert"],
-      secure: process.env.NODE_ENV === "development" ? false : true,
+      sameSite: "lax", // corrected here
+      secrets: ["big_secret"],
+      secure: process.env.NODE_ENV === "production",
     },
   });
 
